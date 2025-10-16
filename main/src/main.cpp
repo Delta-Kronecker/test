82d94944a515d7ea769efde0a88488d0414504f1
Ahmad <diracdeltain0@gmail.com>
2025-10-17 02:14:18 +0330

    Update main.cpp

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,9 +1,15 @@
 #include <QCoreApplication>
 #include <QDebug>
 #include <QDir>
+#include <QMap>
 #include "../include/Utils.h"
 #include "../include/HttpHelper.h"
 #include "../include/SubParser.h"
+
+// Generate unique key for deduplication (server:port+type)
+QString GenerateConfigKey(const std::shared_ptr<ProxyBean> &bean) {
+    return QString("%1:%2+%3").arg(bean->serverAddress).arg(bean->serverPort).arg(bean->type);
+}
 
 int main(int argc, char *argv[]) {
     QCoreApplication app(argc, argv);
@@ -24,7 +30,11 @@ int main(int argc, char *argv[]) {
     qDebug() << "Found" << subLinks.size() << "subscription links";
 
     int totalConfigs = 0;
+    int duplicateCount = 0;
     int configIndex = 1;
+
+    // HashMap for deduplication (key -> bean)
+    QMap<QString, std::shared_ptr<ProxyBean>> uniqueConfigs;
 
     // Process each subscription link
     for (int i = 0; i < subLinks.size(); i++) {
@@ -47,25 +57,42 @@ int main(int argc, char *argv[]) {
         auto beans = SubParser::ParseSubscription(QString::fromUtf8(response.data));
         qDebug() << "  Parsed" << beans.size() << "configs";
 
-        // Save each config as JSON
+        // Add to unique configs map
         for (const auto &bean : beans) {
-            auto json = bean->ToJson();
-            auto jsonStr = QJsonObject2QString(json, false);
+            QString key = GenerateConfigKey(bean);
 
-            QString filename = QString("../../data/Config/config_%1.json").arg(configIndex, 4, 10, QChar('0'));
-
-            if (WriteFileText(filename, jsonStr)) {
-                qDebug() << "  Saved:" << filename << "-" << bean->name;
-                configIndex++;
-                totalConfigs++;
+            if (uniqueConfigs.contains(key)) {
+                // Duplicate found, skip
+                duplicateCount++;
             } else {
-                qDebug() << "  Error saving:" << filename;
+                // New unique config, add to map
+                uniqueConfigs[key] = bean;
+                totalConfigs++;
             }
         }
     }
 
+    qDebug() << "\n=== Saving Unique Configs ===";
+    qDebug() << "Total unique configs:" << totalConfigs;
+    qDebug() << "Duplicates removed:" << duplicateCount;
+
+    // Save unique configs to files
+    for (const auto &bean : uniqueConfigs) {
+        auto json = bean->ToJson();
+        auto jsonStr = QJsonObject2QString(json, false);
+
+        QString filename = QString("../../data/Config/config_%1.json").arg(configIndex, 4, 10, QChar('0'));
+
+        if (WriteFileText(filename, jsonStr)) {
+            configIndex++;
+        } else {
+            qDebug() << "  Error saving:" << filename;
+        }
+    }
+
     qDebug() << "\n=== ConfigCollector Finished ===";
-    qDebug() << "Total configs saved:" << totalConfigs;
+    qDebug() << "Total unique configs saved:" << totalConfigs;
+    qDebug() << "Total duplicates removed:" << duplicateCount;
 
     return 0;
 }
